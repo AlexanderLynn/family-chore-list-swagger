a645363e8778397332fe43afc2686b67ef50dd38
AlexanderLynn <30274747+AlexanderLynn@users.noreply.github.com>
2022-03-09 19:54:28 -0700

    port fix v3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var PATH = process.env.PORT || 5000;
+var PATH = process.env.PORT || 32520;
 
 // swaggerRouter configuration
 var options = {
